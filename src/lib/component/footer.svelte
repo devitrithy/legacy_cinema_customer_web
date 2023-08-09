462b11725674245694c0ee2ce057bde94e0b5896
devit2000 <ditway1405@gmail.com>
2023-08-10 03:23:39 +0700

    first commit

---
+++ src/lib/component/footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {
+    Footer,
+    FooterCopyright,
+    FooterLinkGroup,
+    FooterLink,
+  } from "flowbite-svelte";
+</script>
+
+<Footer class="absolute bottom-0 left-0 z-20 w-full">
+  <FooterCopyright href="/" by="Flowbite™" year={2022} />
+  <FooterLinkGroup
+    ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+  >
+    <FooterLink href="/">About</FooterLink>
+    <FooterLink href="/">Privacy Policy</FooterLink>
+    <FooterLink href="/">Licensing</FooterLink>
+    <FooterLink href="/">Contact</FooterLink>
+  </FooterLinkGroup>
+</Footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.postcss";
+  import Navbar from "$lib/component/navbar.svelte";
+  import Footer from "$lib/component/footer.svelte";
+</script>
+
+<Navbar />
+<div class="flex px-4 mx-auto min-h-[93vh] w-full dark:bg-slate-950">
+  <main class="lg:ml-72 w-full mx-auto">
+    <slot />
+  </main>
+</div>
+<Footer />
