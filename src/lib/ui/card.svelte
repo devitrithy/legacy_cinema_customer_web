a6af1c5b6e191a90f0bd70de97803707b873775d
devit2000 <ditway1405@gmail.com>
2023-08-10 15:32:51 +0700

    change on hover

--- src/lib/ui/card.svelte
+++ src/lib/ui/card.svelte
@@ -13,19 +13,19 @@
     height="360"
     src={`${endpoint}/thumbnail/${url}?w=200&h=300`}
     alt=""
-    class="absolute group-hover:opacity-60"
+    class="absolute group-hover:opacity-60 group-hover:blur-[1px] transition"
   />
   <button id="hover" class="absolute text-2xl hidden group-hover:block m-10">
     <img src={play} alt="button" width="50" srcset="" />
   </button>
-  <Tooltip triggeredBy={`#hover`} class="z-50">Play Trailer</Tooltip>
+  <Tooltip triggeredBy="#hover" class="z-50">Play Trailer</Tooltip>
 
   <button
     class="absolute bottom-0 left-0 right-0 hidden group-hover:block bg-black uppercase py-2 opacity-95"
     >info</button
   >
   <button
-    class="absolute bottom-11 left-0 right-0 hidden group-hover:block bg-primary-900 uppercase py-2 opacity-95"
+    class="absolute bottom-11 left-0 right-0 hidden group-hover:block bg-red-950 uppercase py-2 opacity-95"
     >Booking</button
   >
 </div>
