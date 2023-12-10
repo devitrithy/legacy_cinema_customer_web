3ae2b5127feeda93d708d4ca0852ab438dde51d2
devit2000 <ditway1405@gmail.com>
2023-12-10 16:09:26 +0700

    error contact me

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,8 +3,14 @@
   import internal from "./website-maintenance.gif";
   import e from "./security-research.gif";
   import { DarkMode } from "flowbite-svelte";
+  import {
+    FacebookSolid,
+    PapperPlaneSolid,
+    PhoneSolid,
+  } from "flowbite-svelte-icons";
 </script>
 
+<title>Legacy Cinema | Error {$page.status}</title>
 <div style="display: none;">
   <DarkMode />
 </div>
@@ -16,15 +22,38 @@
       <p class="mb-5 flex flex-col justify-center items-center">
         {#if $page.status === 500}
           <img src={internal} alt="this slowpoke moves" width="550" />
+          <span class="text-3xl"> Server is undermaintained. </span>
+          Please make sure contact me through
+          <div class="flex mt-10 gap-5">
+            <a
+              href="tel:+8558124798"
+              title="Phone Number"
+              class="border border-stone-50 p-3 rounded-full"><PhoneSolid /></a
+            >
+            <a
+              class="border border-stone-50 p-3 rounded-full"
+              title="Facebook"
+              href="https://web.facebook.com/deviiiixD/"
+              target="_blank"
+              rel="noopener noreferrer"><FacebookSolid /></a
+            >
+            <a
+              class="border border-stone-50 p-3 rounded-full"
+              title="Telegram"
+              href="https://t.me/+855884124798"
+              target="_blank"
+              rel="noopener noreferrer"><PapperPlaneSolid /></a
+            >
+          </div>
         {:else}
           <img src={e} alt="this slowpoke moves" width="550" />
+          <span class="text-3xl">
+            {$page.status}
+            {$page.error?.message}
+          </span>
+          <a href="/" class="underline">Click here to go to home page</a>
         {/if}
-        <span class="text-3xl">
-          {$page.status}
-          {$page.error?.message}
-        </span>
       </p>
-      <a href="/" class="underline">Click here to go to home page</a>
     </div>
   </div>
 </div>
